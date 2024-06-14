248e4e87c43ffa59c00781b5a58826d27d195a25
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-06-14 15:54:52 +0200

    add UsersController tests

---
+++ tests/Feature/Controllers/UsersControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class UsersControllerTest extends TestCase
+{
+    public function testCreateUserNoData(): void
+    {
+        $response = $this
+            ->json("POST", "/api/v1/users", [], []);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    public function testCreateUsergoodData(): void
+    {
+        $response = $this
+            ->json("POST", "/api/v1/users", [
+                "firstName" => "asdf",
+                "lastName" => "asdf",
+                "username" => "asdf",
+                "email" => "asdf-asdf@example.com",
+                "password" => "asdf-asdf",
+                "reTypedPassword" => "asdf-sadf",
+            ], []);
+
+        $response
+            ->assertStatus(Response::HTTP_CREATED);
+    }
+}
