e9491ec62dbb776bcc79ae03800c077c8e2575f0
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-06-14 15:15:30 +0200

    sign up form and API routes

--- resources/views/pages/frontend/sign-up.blade.php
+++ resources/views/pages/frontend/sign-up.blade.php
@@ -1,4 +1,30 @@
 @extends("layouts.frontend.layout")
 @section("content")
-Sign up
+    <form method="POST" action="/api/v1/users">
+
+        <div>
+            <input type="text" placeholder="Vezetéknév: *">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Keresztnév: *">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Felhasználónév: *">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Jelszó: *">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Jelszó újra: *">
+        </div>
+
+        @csrf
+        <div>
+            <button type="submit">Regisztráció</button>
+        </div>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,19 @@ use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * App routes
+ */
+
 Route::get('/', [HomeController::class, "home"]);
 Route::get('/sign-up', [FrontendController::class, "signUp"]);
 Route::get('/sign-in', [FrontendController::class, "signIn"]);
+
+
+/**
+ * API routes
+ */
+
+Route::post('/api/v1/users', function () {
+	echo "Hello";
+});
